82ec77f16ffb10cd566da0795f0470fbbf8c664e
SausCode <jicunnin@calpoly.edu>
2018-07-31 21:39:48 -0700

    Code formatting

--- sketch_jul22a.ino
+++ sketch_jul22a.ino
@@ -6,17 +6,18 @@
  */
 
 #include "helper.hpp"
- 
 #include <FiniteStateMachine.h>  // Include finite state machine
 #include <Button.h>              // Include Button
+
+#define ENC 2 //defines interrupt pin
+#define ENDSTOP 4
+#define MOTOR_BREAK 9
+#define MOTOR_DIR 12
  
 const byte NUMBER_OF_STATES = 5; //how many states are we cycling through?
-
 Button endstop = Button(4,PULLUP); //initialize the button (wire between pin 4 and ground)
 byte switchPresses = 0;            //counter variable, holds number of button presses 
-
 unsigned long ticks = 0; //initializes counter variable for encoder
-const int enc_pin = 2;   //defines interrupt pin
 int target = 0;            //number of steps required
  
 //initialize states
@@ -26,38 +27,39 @@ State TouchOff = State(TouchOffV);  // Moving up to touch down again
 State OffWait = State(OffWaitV);    // Waiting after switch is disengaged
 State UpCount = State(UpCountV);    // Moving up to position, counting steps
 
-  if(endstop.isPressed()){
-  FSM sightStateMachine = FSM(TouchOff);     //initialize state machine
-  switchPresses = 1; 
-}else{
-  FSM sightStateMachine = FSM(Homing);     //initialize state machine
+if(endstop.isPressed()){
+	FSM sightStateMachine = FSM(TouchOff);     //initialize state machine
+	switchPresses = 1; 
+}
+else{
+	FSM sightStateMachine = FSM(Homing);     //initialize state machine
 }
 
 // Setup declares pin modes necessary for motor driver shield channel A, the endstop, and the interrupt pin, and begins serial communication.
 // It then runs through the homing process, calling each function as the endstop is pressed and unpressed.
 
 void setup(){
-  pinMode(9,OUTPUT);   //Motor brake pin
-  pinMode(12,OUTPUT);  // Motor direction pin
-  pinMode(4,INPUT);    // Endstop pin 4
-  pinMode(enc_pin,INPUT);
+	pinMode(MOTOR_BREAK,OUTPUT);  //Motor brake pin
+	pinMode(MOTOR_DIR,OUTPUT);    // Motor direction pin
+	pinMode(ENDSTOP,INPUT);       // Endstop pin 4
+	pinMode(ENC,INPUT);
   
-  Serial.begin(9600);
-  attachInterrupt(digitalPinToInterrupt(enc_pin), countTicksV, CHANGE);
+	Serial.begin(9600);
+	attachInterrupt(digitalPinToInterrupt(enc_pin), countTicksV, CHANGE);
 
-while(switchPresses<=4){
-if(endstop.stateChanged()){ 
-  switchPresses = ++switchPresses;
-  switch(switchPresses){
-    case 0: sightStateMachine.immediateTransitionTo(Homing);
-    case 1: sightStateMachine.immediateTransitionTo(TouchOff);
-    case 2: sightStateMachine.immediateTransitionTo(OffWait);
-    case 3: sightStateMachine.immediateTransitionTo(Homing);
-    case 4: sightStateMachine.immediateTransitionTo(inPos);
-    break;
-      }
-}
-}
+	while(switchPresses<=4){
+		if(endstop.stateChanged()){ 
+			switchPresses = ++switchPresses;
+			switch(switchPresses){
+				case 0: sightStateMachine.immediateTransitionTo(Homing);
+				case 1: sightStateMachine.immediateTransitionTo(TouchOff);
+				case 2: sightStateMachine.immediateTransitionTo(OffWait);
+				case 3: sightStateMachine.immediateTransitionTo(Homing);
+				case 4: sightStateMachine.immediateTransitionTo(inPos);
+				break;
+		  	}
+		}
+	}
 }
 
 // Loop takes in serial inputs and defines target as number of ticks needed to reach the desired point on sight. Conversion formulas still need to be added in this step to convert encoder ticks to sight markings. This can be done in
@@ -65,28 +67,29 @@ if(endstop.stateChanged()){
 // The first if statement defines switchPresses depending on if the endstop switch is currently pressed or not. This defines the initial switch case in the upcoming switch function. The state machine cycles through the cases until UpCount which
 // begins moving the sight up to its desired position. The final if statement transitions the state machine to inPos to stop the sight in the target position once the desired amount of encoder ticks have been reached.
 void loop(){
-  if (Serial.available()){
-    target = Serial.read();
-    
-  if(isPressed(endstop)){
-    switchPresses = 1; 
-   }else{switchPresses = 0;}
-   
-while(switchPresses<=4){
-if(endstop.stateChanged()){ 
-  switchPresses = ++switchPresses;
-  switch(switchPresses){
-    case 0: sightStateMachine.immediateTransitionTo(Homing);
-    case 1: sightStateMachine.immediateTransitionTo(TouchOff);
-    case 2: sightStateMachine.immediateTransitionTo(OffWait);
-    case 3: sightStateMachine.immediateTransitionTo(Homing);
-    case 4: sightStateMachine.immediateTransitionTo(UpCount);
-    break;}
-      }
-  }
-if (ticks>=target){
-  sightStateMachine.immediateTransitionTo(inPos);  
+	if (Serial.available()){
+		target = Serial.read();
+		if(isPressed(endstop)){
+			switchPresses = 1; 
+		}
+		else{
+			switchPresses = 0;
+		}
+		while(switchPresses<=4){
+			if(endstop.stateChanged()){ 
+				switchPresses = ++switchPresses;
+				switch(switchPresses){
+		  			case 0: sightStateMachine.immediateTransitionTo(Homing);
+		  			case 1: sightStateMachine.immediateTransitionTo(TouchOff);
+		  			case 2: sightStateMachine.immediateTransitionTo(OffWait);
+		  			case 3: sightStateMachine.immediateTransitionTo(Homing);
+		  			case 4: sightStateMachine.immediateTransitionTo(UpCount);
+		  			break;
+		  		}
+			}
+		}
+		if (ticks>=target){
+			sightStateMachine.immediateTransitionTo(inPos);  
+		}
+	}  
 }
-  }  
-}
-
